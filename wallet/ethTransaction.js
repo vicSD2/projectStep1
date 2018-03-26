31a958df7c677218245e22d9844ce71e62b652a0
Pablo Peillard <pablo12335@gmail.com>
2018-03-26 17:53:48 -0500

    initial wallet code CLI

--- wallet/ethTransaction.js
+++ wallet/ethTransaction.js
@@ -17,36 +17,52 @@ web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/YOUR-
 // An extra module is required for this, use npm to install before running
 var Tx = require('ethereumjs-tx');
 
+var pubKey = "0x8a0e3931463b71050033253af4e5e35a95b19b38"
 // Used to sign the transaction. Obviously you SHOULD better secure this than just plain text
 var privateKey = new Buffer('82f7a41925b65cd45338989c3aa16967cd08632934eeef39dc302ac0ed40c026', 'hex');
+var publicKey = new Buffer(pubKey, 'hex');
 
- // The reciviing address of the transaction
-var receivingAddr = ('0x9Ff24857b990a8d036D0973d9a0825A76C2c8723');
-
-// Value to be sent, converted to wei and then into a hex value
-var txValue = web3.utils.numberToHex(web3.utils.toWei('0.1', 'ether'));
-
-// Data to be sent in transaction, converted into a hex value. Normal tx's do not need this and use '0x' as default, but who wants to be normal?
-var txData = web3.utils.asciiToHex('oh hai mark'); 
-
-var rawTx = {
-  nonce: '0x0', // Nonce is the times the address has transacted, should always be higher than the last nonce 0x0#
-  gasPrice: '0x14f46b0400', // Normal is '0x14f46b0400' or 90 GWei
-  gasLimit: '0x55f0', // Limit to be used by the transaction, default is '0x55f0' or 22000 GWei
-  to: receivingAddr, // The receiving address of this transaction
-  value: txValue, // The value we are sending '0x16345785d8a0000' which is 0.1 Ether
-  data: txData // The data to be sent with transaction, '0x6f6820686169206d61726b' or 'oh hai mark' 
+if (!process.argv[2]) {
+    console.log("Missing Address");
 }
 
-//console.log(rawTx); // This is used for testing to see if the rawTx was formmated created properly, comment out the code below to use.
+if (!process.argv[3]) {
+    console.log("Amount");
+}
+
+var number = web3.eth.getTransactionCount(pubKey);
+
+number.then(function(numb) {
+	numb +=1;
+    // The reciviing address of the transaction
+    var receivingAddr = (process.argv[2]); // '0xb2d180dc3c55e57783c0351fd0279d779aa63286'
+
+    // Value to be sent, converted to wei and then into a hex value
+    var txValue = web3.utils.numberToHex(web3.utils.toWei(process.argv[3], 'ether'));
+
+    // Data to be sent in transaction, converted into a hex value. Normal tx's do not need this and use '0x' as default, but who wants to be normal?
+    var txData = web3.utils.asciiToHex('oh hai vic');
+
+    var rawTx = {
+        nonce: "0x"+numb, // Nonce is the times the address has transacted, should always be higher than the last nonce 0x0#
+        gasPrice: '0x14f46b0400', // Normal is '0x14f46b0400' or 90 GWei
+        gasLimit: '0x55f0', // Limit to be used by the transaction, default is '0x55f0' or 22000 GWei
+        to: receivingAddr, // The receiving address of this transaction
+        value: txValue, // The value we are sending '0x16345785d8a0000' which is 0.1 Ether
+        data: txData // The data to be sent with transaction, '0x6f6820686169206d61726b' or 'oh hai mark' 
+    }
+
+    //console.log(rawTx); // This is used for testing to see if the rawTx was formmated created properly, comment out the code below to use.
 
 
-var tx = new Tx(rawTx);
-tx.sign(privateKey); // Here we sign the transaction with the private key
+    var tx = new Tx(rawTx);
+    tx.sign(privateKey); // Here we sign the transaction with the private key
 
-var serializedTx = tx.serialize(); // Clean things up a bit
+    var serializedTx = tx.serialize(); // Clean things up a bit
 
-console.log(serializedTx.toString('hex')); // Log the resulting raw transaction hex for debugging if it fails to send
+    console.log(serializedTx.toString('hex')); // Log the resulting raw transaction hex for debugging if it fails to send
 
-web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')) // Broadcast the transaction to the network
-.on('receipt', console.log); // When a receipt is issued, log it to the console
+    web3.eth.sendSignedTransaction('0x' + serializedTx.toString('hex')) // Broadcast the transaction to the network
+        .on('receipt', console.log); // When a receipt is issued, log it to the console
+
+});
